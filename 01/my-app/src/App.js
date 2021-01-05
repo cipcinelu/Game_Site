0da4881b2ebfa5fadab20deba8cd05a04756b24f
Steve Yoba <cipcinelu@yandex.ru>
2021-01-05 19:43:01 +0300

    main

--- 01/my-app/src/App.js
+++ 01/my-app/src/App.js
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <div className="app-wrapper">
       <Header></Header>
-      ХУЙ ПИЗДА БОРОДА
+      откатили
       <Navbar></Navbar>
       <Profile></Profile>
     </div>
