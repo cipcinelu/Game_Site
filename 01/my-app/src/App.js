1776adb784d2c06cc8147a4678a71fd081f31d03
Steve Yoba <cipcinelu@yandex.ru>
2021-04-21 22:33:34 +0300

    master

--- 01/my-app/src/App.js
+++ 01/my-app/src/App.js
@@ -3,19 +3,19 @@ import Header from './components/Header/Header';
 import Navbar from './components/NavBar/NavBar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
-import {BrowserRouter, Route} from 'react-router-dom'
+import {Route} from 'react-router-dom'
+import { updateNewPostText } from './redux/state';
 
 const App = (props) => {
-  return (<BrowserRouter>
+  return (
     <div className="app-wrapper">
       <Header></Header>
       <Navbar></Navbar>
       <div class = 'app-wrapper-content'>
-        <Route path='/dialogs' render = {() => <Dialogs dialogs = {props.dialogs} messages = {props.messages}/>}></Route>
-        <Route path='/profile' render = {() => <Profile posts = {props.posts}/>}></Route>
+        <Route path='/dialogs' render = {() => <Dialogs state = {props.state.dialogsPage}/>}></Route>
+        <Route path='/profile' render = {() => <Profile profilePage = {props.state.profilePage} addPost = {props.addPost} updateNewPostText = {props.updateNewPostText}/>}></Route>
       </div>
     </div>
-    </BrowserRouter>
   );
 }
 
